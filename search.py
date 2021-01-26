cbdcc532336432c98b33ece5e1dac9399d872c43
ullahhab <61397521+ullahhab@users.noreply.github.com>
2021-01-26 15:02:26 -0800

    Add files via upload

--- search.py
+++ search.py
@@ -1,32 +1,36 @@
-
-
-
-
 import os
 isFile = ['configure','Makefile','vif-core','core-daemon','compile','missing','install-sh','depcomp','core-gui','README','freebsd8-config-CORE','Changelog','freebsd7-config-CORE','freebsd7-config-COREDEBUG','changelog','control','compat','rules','copyright','README-Xen','sampleIPsec','sampleIPsec','sampleVPNServer','sampleFirewall','sampleVPNClient','controlnet_updown','LICENSE','vnoded','netns','vcmd','core-manage','core-manage','core-cleanup','core-xen-cleanup','coresendmsg','coresendmsg','stamp-h1']
 Dir = os.getcwd()+'/core-rel-4.8'
 explored=0
-def pDir(Dir):
+word = input("Please type the word ")
+def pDir(Dir,word):
 	explored=0
-	for i in os.listdir(Dir):
-		explored +=1
-		if (len(i.split("."))>=2) or (i in isFile):
-			#pfile(Dir+'/'+i)
-			print(Dir+'/'+i)
-			#print(Dir)
-		else:
-			print("debug")
-			#pfile(Dir+'/'+i)
-			pDir(Dir+'/'+i)
-			print(i,explored)
+	try:
+		for i in os.listdir(Dir):
+			explored +=1
+			if (len(i.split("."))>=2) or (i in isFile):
+				pfile(Dir+'/'+i,word)
+				#print(Dir+'/'+i)
+				#print(Dir)
+			else:
+				#print("debug")
+				#pfile(Dir+'/'+i)
+				pDir(Dir+'/'+i,word)
+				#print(i,explored)
+	except: 
+		os.system("cat " +Dir +" | grep "+word)
 
-def pfile(filename):
-	File = open(filename,'r')
-	File = File.read()
-	for i in File:
-		print(i)
+def pfile(filename,word):
+	try:
+		File = open(filename,'r')
+		print("--------------"+filename+"------------")
+		os.system("cat " + filename +" | grep "+word)
+		#os.system("cat "+filename)
+	except:
+		pDir(filename,word)
+		
 
-#print(pDir(Dir))
+print(pDir(Dir,word))
 
 
 def getsubdir(Dir):
@@ -49,10 +53,7 @@ try:
 	filename = open(dir+'/'+i,'r')
 except 
 	pfile(dir+'/'+i,'r') i should really be added with dir
-
 This simple change will allow it to search throug any file and any directory as it will be able to distinguish between file and directory
 '''
 #getsubdir(5)
-filename = open(Dir, 'r')
-		
-
+#filename = open(Dir, 'r')
